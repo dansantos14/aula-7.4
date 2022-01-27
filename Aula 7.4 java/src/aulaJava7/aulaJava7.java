52b70ddcc2ae9dc8ad2409eb634a6ec4563dd7fd
Danilo Augusto dos Santos <dansantos14@gmail.com>
2022-01-26 21:44:57 -0300

    aula 7.4 curso java

---
+++ Aula 7.4 java/src/aulaJava7/aulaJava7.java
@@ -0,0 +1,20 @@
+package aulaJava7;
+
+public class aulaJava7 {
+
+	public static void main(String[] args) {
+		
+		int maiorIdade = 18;
+		
+		int idosoIdade = 60;
+		
+		int jovemIdade = 16;
+		
+		int criancaIdade = 7;
+		
+		System.out.println("Maior idade é = " + maiorIdade);
+		
+			
+	}
+
+}
