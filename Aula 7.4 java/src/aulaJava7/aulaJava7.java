5bd293997abd683fd055508ecba33b5e3b1999c3
Danilo Augusto dos Santos <dansantos14@gmail.com>
2022-01-26 21:59:41 -0300

    comitando aula 7 - 26012022

--- Aula 7.4 java/src/aulaJava7/aulaJava7.java
+++ Aula 7.4 java/src/aulaJava7/aulaJava7.java
@@ -3,18 +3,20 @@ package aulaJava7;
 public class aulaJava7 {
 
 	public static void main(String[] args) {
+	
+	int maiorIdade = 18;
+	
+	int idosoIdade = 60;
+	
+	int jovemIdade = 16;
+	
+	int criancaIdade = 7;
+	
+	System.out.println("Idoso idade é = " + idosoIdade);
+	System.out.println("Maior idade é = " + maiorIdade);
+	System.out.println("Jovem idade é = " + jovemIdade);
+	System.out.println("Criança idade é = " + criancaIdade);
 		
-		int maiorIdade = 18;
-		
-		int idosoIdade = 60;
-		
-		int jovemIdade = 16;
-		
-		int criancaIdade = 7;
-		
-		System.out.println("Maior idade é = " + maiorIdade);
-		
-			
 	}
 
 }
